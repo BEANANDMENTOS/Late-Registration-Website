7307e9ed604da8020354177f52adaa9c0fbef9c4
BEANANDMENTOS <bean123bbb@gmail.com>
2020-11-22 14:41:57 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
+var https = require("https");
+var http = require("https");
+const db = require('monk')("localhost:27017/InformationDB")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,5 +22,24 @@ router.get('/attdoc', function (req, res, next) {
   res.render('attdoc', { title: 'good' });
 });
 
+router.get('/addsubject', function (req, res, next) {
+  res.render('addsubject', { title: 'good' });
+});
+
+router.get('/bts', function (req, res, next) {
+  res.render('BTS x ARMY', { title: 'YEAH' });
+});
+
+router.get('/information', function (req, res, next) {
+  res.render('information', { title: 'YEAH' });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('new', { title: 'YEAH' });
+});
+
+router.post("/login", async (req, res) => {
+  //api
+});
 
 module.exports = router;
