106048f40e7b56168b9e2a8dc3742c230256c3a2
BEANANDMENTOS <bean123bbb@gmail.com>
2020-11-24 05:13:32 +0700

    update

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var db = require("./db")
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +12,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.use(cors())
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
--- routes/index.js
+++ routes/index.js
@@ -3,51 +3,242 @@ var router = express.Router();
 var path = require("path");
 var https = require("https");
 var http = require("https");
-const db = require('monk')("localhost:27017/InformationDB")
+
+
+let nameTH = "นาวี ฤทธิ์เดช";
+let nameEng = "Nave Ritdeath"
+let ID = "6209650000"
+
+router.post('/login', async function(req, res) {
+  console.log(req.body);
+  if(req.body.username == '6209650222' && req.body.password == '1759900375116') {
+    res.render('home', {
+      status: true,
+      message: "Success",
+      type: "student",
+      username: "6209650222",
+      tu_status: "ปกติ",
+      statusid: "10",
+      displayname_th: "ภูริน โถทอง",
+      displayname_en: "POORIN THOTONG",
+      email: "poorin.tho@dome.tu.ac.th",
+      department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+      faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+    });
+  }
+  else if(req.body.username == 'admin' && req.body.password == 'eiei')  {
+    res.render('home', {
+      status: true,
+      message: "Success",
+      type: "employee",
+      username: "",
+      tu_status: "ปกติ",
+      statusid: "10",
+      displayname_th: "พี่ริน",
+      displayname_en: "P' Rin",
+      email: "EIEI@dome.tu.ac.th",
+      department: "",
+      faculty: "",
+    });
+  }
+
+  else res.render('login')
+});
+
+// const getlogin = (userName, password) => {
+//   return new Promise((resolve, reject) => {
+//       var options = {
+//           'method': 'POST',
+//           'hostname': 'restapi.tu.ac.th',
+//           'path': '/api/v1/auth/Ad/verify',
+//           'headers': {
+//               'Content-Type': 'application/json',
+//               'Application-Key': 'TU9802230512e7ad2588c067a8474d515ddd88667a6a1f36e2eb5f6b1480371e60ab63c3f75a3514828cf132b4dfe07779'
+//           }
+//       };
+//       var req = https.request(options, (res) => {
+//           var chunks = [];
+//           res.on("data", function (chunk) {
+//               chunks.push(chunk);
+//           });
+//           res.on("end", function (chunk) {
+//               var body = Buffer.concat(chunks);
+//               //result = body;
+//               resolve(body.toString());
+//               //result = chunks;
+//           });
+//           res.on("error", function (error) {
+//               console.error(error);
+//               reject(error);
+//           });
+//       });
+//       var postData = "{\n\t\"UserName\":\"" + userName + "\",\n\t\"PassWord\":\"" + password + "\"\n}";
+//       req.write(postData);
+//       req.end();
+//   });
+// };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
-
+router.get('/admin', function(req, res, next) {
+  res.render('admin', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
+});
 router.get('/home', function (req, res, next) {
-  res.render('home', { title: 'nice' });
+  res.render('home', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
 });
 
 router.get('/accInfo', function (req, res, next) {
-  res.render('accInfo', { title: 'nice' });
+  res.render('accInfo', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
 });
 
 router.get('/agreement', function (req, res, next) {
-  res.render('agreement', { title: 'well' });
+  res.render('agreement', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
 });
 
 router.get('/attdoc', function (req, res, next) {
-  res.render('attdoc', { title: 'good' });
+  res.render('attdoc', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
 });
 
 router.get('/addsubject', function (req, res, next) {
-  res.render('addsubject', { title: 'good' });
-});
-
-router.get('/bts', function (req, res, next) {
-  res.render('BTS x ARMY', { title: 'YEAH' });
+  res.render('addsubject', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
 });
 
 router.get('/information', function (req, res, next) {
-  res.render('information', { title: 'YEAH' });
+  res.render('information', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
 });
 
-router.get('/new', function (req, res, next) {
-  res.render('new', { title: 'YEAH' });
+router.get('/contactus', function (req, res, next) {
+  res.render('contactus', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
 });
 
-router.get('/ploy', function (req, res, next) {
-  res.render('ploy', { title: 'YEAH' });
+router.get('/payment', function (req, res, next) {
+  res.render('payment', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
 });
 
-router.post("/login", async (req, res) => {
-  //api
+router.get('/charges', function (req, res, next) {
+  res.render('charges', {
+    status: true,
+    message: "Success",
+    type: "student",
+    username: "6209650222",
+    tu_status: "ปกติ",
+    statusid: "10",
+    displayname_th: "ภูริน โถทอง",
+    displayname_en: "POORIN THOTONG",
+    email: "poorin.tho@dome.tu.ac.th",
+    department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+    faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+  });
 });
 
 module.exports = router;
