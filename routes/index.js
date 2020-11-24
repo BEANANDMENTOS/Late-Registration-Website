1ffda52125e451c0e82d34180e220529b9a34c94
BEANANDMENTOS <bean123bbb@gmail.com>
2020-11-24 16:30:37 +0700

    api suss

--- routes/index.js
+++ routes/index.js
@@ -9,104 +9,104 @@ let nameTH = "นาวี ฤทธิ์เดช";
 let nameEng = "Nave Ritdeath"
 let ID = "6209650000"
 
-router.post('/login', async function(req, res) {
-  console.log(req.body);
-  if(req.body.username == '6209650222' && req.body.password == '1759900375116') {
-    res.render('home', {
-      status: true,
-      message: "Success",
-      type: "student",
-      username: "6209650222",
-      tu_status: "ปกติ",
-      statusid: "10",
-      displayname_th: "ภูริน โถทอง",
-      displayname_en: "POORIN THOTONG",
-      email: "poorin.tho@dome.tu.ac.th",
-      department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
-      faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
-    });
-  }
-  else if(req.body.username == '6209650230' && req.body.password == '1412') {
-    res.render('home', {
-      status: true,
-      message: "Success",
-      type: "student",
-      username: "6209650230",
-      tu_status: "ปกติ",
-      statusid: "10",
-      displayname_th: "นวพงศ์ สิตรุโน",
-      displayname_en: "Nawapong Sitaruno",
-      email: "nawapong.sit@dome.tu.ac.th",
-      department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
-      faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
-    });
-  }
-  else if(req.body.username == 'isus' && req.body.password == 'api') {
-    res.render('home', {
-      status: true,
-      message: "Success",
-      type: "student",
-      username: "6209650222",
-      tu_status: "ปกติ",
-      statusid: "10",
-      displayname_th: "ภูริน โถทอง",
-      displayname_en: "POORIN THOTONG",
-      email: "poorin.tho@dome.tu.ac.th",
-      department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
-      faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
-    });
-  }
-  else if(req.body.username == 'admin' && req.body.password == 'eiei')  {
-    res.render('home', {
-      status: true,
-      message: "Success",
-      type: "employee",
-      username: "",
-      tu_status: "ปกติ",
-      statusid: "10",
-      displayname_th: "พี่ริน",
-      displayname_en: "P' Rin",
-      email: "EIEI@dome.tu.ac.th",
-      department: "",
-      faculty: "",
-    });
-  }
+// router.post('/login', async function(req, res) {
+//   console.log(req.body);
+//   if(req.body.username == '6209650222' && req.body.password == '1759900375116') {
+//     res.render('home', {
+//       status: true,
+//       message: "Success",
+//       type: "student",
+//       username: "6209650222",
+//       tu_status: "ปกติ",
+//       statusid: "10",
+//       displayname_th: "ภูริน โถทอง",
+//       displayname_en: "POORIN THOTONG",
+//       email: "poorin.tho@dome.tu.ac.th",
+//       department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+//       faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+//     });
+//   }
+//   else if(req.body.username == '6209650230' && req.body.password == '1412') {
+//     res.render('home', {
+//       status: true,
+//       message: "Success",
+//       type: "student",
+//       username: "6209650230",
+//       tu_status: "ปกติ",
+//       statusid: "10",
+//       displayname_th: "นวพงศ์ สิตรุโน",
+//       displayname_en: "Nawapong Sitaruno",
+//       email: "nawapong.sit@dome.tu.ac.th",
+//       department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+//       faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+//     });
+//   }
+//   else if(req.body.username == 'isus' && req.body.password == 'api') {
+//     res.render('home', {
+//       status: true,
+//       message: "Success",
+//       type: "student",
+//       username: "6209650222",
+//       tu_status: "ปกติ",
+//       statusid: "10",
+//       displayname_th: "ภูริน โถทอง",
+//       displayname_en: "POORIN THOTONG",
+//       email: "poorin.tho@dome.tu.ac.th",
+//       department: "ปริญญาตรีภาคพิเศษ สาขาวิชาวิทยาการคอมพิวเตอร์",
+//       faculty: "คณะวิทยาศาสตร์และเทคโนโลยี",
+//     });
+//   }
+//   else if(req.body.username == 'admin' && req.body.password == 'eiei')  {
+//     res.render('home', {
+//       status: true,
+//       message: "Success",
+//       type: "employee",
+//       username: "",
+//       tu_status: "ปกติ",
+//       statusid: "10",
+//       displayname_th: "พี่ริน",
+//       displayname_en: "P' Rin",
+//       email: "EIEI@dome.tu.ac.th",
+//       department: "",
+//       faculty: "",
+//     });
+//   }
 
-  else res.render('login')
-});
+//   else res.render('login')
+// });
 
-// const getlogin = (userName, password) => {
-//   return new Promise((resolve, reject) => {
-//       var options = {
-//           'method': 'POST',
-//           'hostname': 'restapi.tu.ac.th',
-//           'path': '/api/v1/auth/Ad/verify',
-//           'headers': {
-//               'Content-Type': 'application/json',
-//               'Application-Key': 'TU9802230512e7ad2588c067a8474d515ddd88667a6a1f36e2eb5f6b1480371e60ab63c3f75a3514828cf132b4dfe07779'
-//           }
-//       };
-//       var req = https.request(options, (res) => {
-//           var chunks = [];
-//           res.on("data", function (chunk) {
-//               chunks.push(chunk);
-//           });
-//           res.on("end", function (chunk) {
-//               var body = Buffer.concat(chunks);
-//               //result = body;
-//               resolve(body.toString());
-//               //result = chunks;
-//           });
-//           res.on("error", function (error) {
-//               console.error(error);
-//               reject(error);
-//           });
-//       });
-//       var postData = "{\n\t\"UserName\":\"" + userName + "\",\n\t\"PassWord\":\"" + password + "\"\n}";
-//       req.write(postData);
-//       req.end();
-//   });
-// };
+const getlogin = (userName, password) => {
+  return new Promise((resolve, reject) => {
+      var options = {
+          'method': 'POST',
+          'hostname': 'restapi.tu.ac.th',
+          'path': '/api/v1/auth/Ad/verify',
+          'headers': {
+              'Content-Type': 'application/json',
+              'Application-Key': 'TUbf2a9e155ac34ffe2d7a6c3a1215758054ec08cede03aa4b7c2bf901b94a7436a479464ca406eef8aa37278c17e9eeb6'
+          }
+      };
+      var req = https.request(options, (res) => {
+          var chunks = [];
+          res.on("data", function (chunk) {
+              chunks.push(chunk);
+          });
+          res.on("end", function (chunk) {
+              var body = Buffer.concat(chunks);
+              //result = body;
+              resolve(body.toString());
+              //result = chunks;
+          });
+          res.on("error", function (error) {
+              console.error(error);
+              reject(error);
+          });
+      });
+      var postData = "{\n\t\"UserName\":\"" + userName + "\",\n\t\"PassWord\":\"" + password + "\"\n}";
+      req.write(postData);
+      req.end();
+  });
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -271,4 +271,39 @@ router.get('/charges', function (req, res, next) {
   });
 });
 
+router.post("/login", async (req, res) => {
+  console.log(req.body);
+    const temp = await getlogin(req.body.username, req.body.password);
+    console.log("temp = " + temp);
+    if (temp) {
+        let j = JSON.parse(temp);
+        console.log(j);
+        if (j.status == true) {
+          username = j.username;
+            displayname_th = j.displayname_th;
+            displayname_en = j.displayname_en;
+            emaill = j.email;
+            facultyy = j.faculty;
+            departmentt = j.department;
+
+            res.render("home",{
+              username : j.username,
+                displayname_th : j.displayname_th,
+                name_endisplayname_en : j.displayname_en,
+                email : j.email,
+                faculty : j.faculty,
+                department : j.department,
+            });
+            //res.send("Name th: " + j.displayname_th);
+        } else {
+            res.render("login");
+        }
+        // if (j.data.status == true) {
+        //   res.send("ʶҹ�:  �ѡ�֡��")
+        // } else (j.data.status)
+        // req.send("ʶҹ�: ��ʶҾ")
+    } else {
+        res.render("login");
+    }
+});
 module.exports = router;
