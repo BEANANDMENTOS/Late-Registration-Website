310fe3325b870713923ac1ed50de827b98caec70
BEANANDMENTOS <bean123bbb@gmail.com>
2021-02-19 13:37:27 +0700

    commit

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
-// const username = "bean_eiei";
-// const password = "123456bbb";
-// const dbName = "cs264DB"
+const username = "bean_eiei";
+const password = "123456bbb";
+const dbName = "cs264DB"
 
-// const mongo_url = `mongodb+srv://${username}:${password}@cluster0.qezk3.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-// mongoose.Promise = global.Promise;
-// mongoose.connect(mongo_url, { useNewUrlParser: true }).then(
-//   () => {
-//     console.log("[success] task 2 : connected to the database ");
-//   },
-//   (error) => {
-//     console.log("[failed] task 2 " + error);
-//     process.exit();
-//   }
-// );
+const mongo_url = `mongodb+srv://${username}:${password}@cluster0.qezk3.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+mongoose.Promise = global.Promise;
+mongoose.connect(mongo_url, { useNewUrlParser: true }).then(
+  () => {
+    console.log("[success] task 2 : connected to the database ");
+  },
+  (error) => {
+    console.log("[failed] task 2 " + error);
+    process.exit();
+  }
+);
